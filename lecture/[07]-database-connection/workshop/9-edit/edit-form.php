2b5d4d900ec660de7ab4d8a7806f24c3f1912420
Mok <mokmaard646@gmail.com>
2024-09-24 21:25:50 +0700

    feat(edit): add php edit end point.

---
+++ lecture/[07]-database-connection/workshop/9-edit/edit-form.php
@@ -0,0 +1,23 @@
+<?php include "../../database-instance.php"?>
+
+<?php
+  $stmt = $pdo->prepare("UPDATE member 
+                         SET 
+                         password=?
+                         name=?
+                         address=?
+                         mobile=?
+                         email=?
+                         WHERE username=?");
+  $stmt->bindParam(6, $_POST['username']);
+  $stmt->bindParam(1, $_POST['password']);
+  $stmt->bindParam(2, $_POST['name']);
+  $stmt->bindParam(3, $_POST['address']);
+  $stmt->bindParam(4, $_POST['mobile']);
+  $stmt->bindParam(5, $_POST['email']);
+
+  if($stmt->execute()){
+    echo "Member.$_POST['username'].has been updated";
+  }
+?>
+
