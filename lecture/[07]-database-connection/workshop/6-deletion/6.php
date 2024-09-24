10ca6fa3a2fbbb201f1d54476a809603a23cd28f
Mok <mokmaard646@gmail.com>
2024-09-24 21:03:36 +0700

    .

---
+++ lecture/[07]-database-connection/workshop/6-deletion/6.php
@@ -0,0 +1,9 @@
+<?php include "../../database-instance.php" ?>
+<?php 
+  $stmt=$pdo->prepare("DELETE FROM member WHERE username = ?");
+  $stmt->bindParam(1, $_GET["username"]);
+
+  if ( $stmt->execute() ) {
+    header("Location: http://202.44.40.193/~cs6520159/phpDBLab8/workshop");
+  }
+?>
---
+++ lecture/[07]-database-connection/workshop/7-insertion/form.php
@@ -0,0 +1,69 @@
+<?php include "../database-instance.php" ?>
+<html>
+<head><meta charset="utf-8">
+        <style>
+                table,td, th {
+                        border: 1px solid black;}
+        </style>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="p-4">
+  <h1 class="text-4xl py-8 ">Add Users</h1>
+
+  <form action="./insert.php" class="grid grid-cols-2 grid-rows-4 gap-4" method="post">
+    <div class="flex items-center gap-2">
+      <label for="name">
+        name
+      </label>
+      <input type="text" name="name" id="name"  class="border-2" value="Mok Maard">
+    </div>
+
+    <div class="flex items-center gap-2">
+      <label for="username">
+        username
+      </label>
+      <input type="text" name="username" id="username"  class="border-2" value="Mok">
+    </div>
+
+    <div class="flex items-center gap-2">
+      <label for="password">
+        passsword
+      </label>
+      <input type="password" name="password" id="password"  class="border-2" value="123">
+    </div>
+
+    <div class="col-span-2 gap-2">
+      <label for="address">
+        address
+      </label>
+      <textarea name="address" id="address" class="border-2 w-full">
+        24/198
+      </textarea>
+    </div>
+
+    <div class="flex items-center gap-2">
+      <label for="mobile">
+        mobile
+      </label>
+      <input type="tel" name="mobile"  class="border-2" value="0863102312">
+    </div>
+
+
+    <div class="flex items-center gap-2">
+      <label for="email
+      ">email</label>
+      <input type="email" name="email"  class="border-2" value="mok@gmail.com">
+    </div>
+
+    
+    <div class="cols-span-2">
+      <button type="submit"
+              class="bg-red-200 p-2 rounded-md hover:bg-red-300 h-fit"> 
+        + Add user
+      </button>
+    </div>
+  </form>
+</body>
+</html>
