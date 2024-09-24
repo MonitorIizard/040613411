724b1b8d37137542b917f0cd65889105572b0806
Mok <mokmaard646@gmail.com>
2024-09-24 22:06:53 +0700

    feat(edit form): assign default value from fetching

--- lecture/[07]-database-connection/workshop/9-edit/form.php
+++ lecture/[07]-database-connection/workshop/9-edit/form.php
@@ -1,4 +1,4 @@
-<?php include "../database-instance.php" ?>
+<?php include "../../database-instance.php" ?>
 <html>
 <head><meta charset="utf-8">
         <style>
@@ -9,29 +9,35 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="p-4">
-  <h1 class="text-4xl py-8 font-bold">Add Users</h1>
+<?php $stmt=$pdo->prepare("SELECT * FROM member WHERE username=?");
+      $stmt->bindParam(1,$_GET['username']);
+      $stmt->execute();
+      $member=$stmt->fetch();
+?>
 
-  <form action="./insert.php" class="flex flex-col gap-4" method="post">
+<body class="p-4">
+  <h1 class="text-4xl py-8 font-bold">Edit Users</h1>
+
+  <form action="./handle.php" class="flex flex-col gap-4" method="post">
     <div class="flex flex-col ">
       <label for="name" class="font-bold">
         name:
       </label>
-      <input type="text" name="name" id="name"  class="border-2 rounded-md p-1 pl-2">
+      <input type="text" name="name" id="name"  class="border-2 rounded-md p-1 pl-2" value="<?=$member['name']?>">
     </div>
 
     <div class="flex flex-col">
       <label for="username" class="font-bold">
         username :
       </label>
-      <input type="text" name="username" id="username"   class="border-2 rounded-md p-1 pl-2">
+      <input type="text" name="username" id="username"   class="border-2 rounded-md p-1 pl-2" value="<?=$member['username']?>">
     </div>
 
     <div class="flex flex-col ">
       <label for="password" class="font-bold">
         passsword :
       </label>
-      <input type="password" name="password" id="password"   class="border-2 rounded-md p-1 pl-2">
+      <input type="password" name="password" id="password"   class="border-2 rounded-md p-1 pl-2" value="<?=$member['password']?>">
     </div>
 
     <div class="flex flex-col "">
@@ -39,6 +45,7 @@
         address :
       </label>
       <textarea name="address" id="address"  class="border-2 rounded-md p-1 pl-2">
+        <?=$member['address']?>"
       </textarea>
     </div>
 
@@ -46,21 +53,21 @@
       <label for="mobile" class="font-bold">
         mobile :
       </label>
-      <input type="tel" name="mobile"   class="border-2 rounded-md p-1 pl-2">
+      <input type="tel" name="mobile"   class="border-2 rounded-md p-1 pl-2" value="<?=$member['mobile']?>">
     </div>
 
 
     <div class="flex flex-col">
       <label for="email" class="font-bold">email</label>
-      <input type="email" name="email"   class="border-2 rounded-md p-1 pl-2">
+      <input type="email" name="email"   class="border-2 rounded-md p-1 pl-2" value="<?=$member['email']?>">
     </div>
 
     
     <div class="cols-span-2">
       <button type="submit"
-              class="bg-red-200 p-2 rounded-md hover:bg-red-300 h-fit
+              class="bg-blue-200 p-2 rounded-md hover:bg-blue-300 h-fit
                      font-bold"> 
-        + Add user
+        ^ Edit user
       </button>
     </div>
   </form>
