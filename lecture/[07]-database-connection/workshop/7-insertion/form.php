8c6fc9221a3b1b26529b0802728c783558466a64
Mok <mokmaard646@gmail.com>
2024-09-24 21:42:04 +0700

    refactore(decorate): decorate form with tailwind.

--- lecture/[07]-database-connection/workshop/7-insertion/form.php
+++ lecture/[07]-database-connection/workshop/7-insertion/form.php
@@ -10,56 +10,56 @@
 </head>
 
 <body class="p-4">
-  <h1 class="text-4xl py-8 ">Add Users</h1>
+  <h1 class="text-4xl py-8 font-bold">Add Users</h1>
 
-  <form action="./insert.php" class="grid grid-cols-2 grid-rows-4 gap-4" method="post">
-    <div class="flex items-center gap-2">
-      <label for="name">
-        name
+  <form action="./insert.php" class="flex flex-col gap-4" method="post">
+    <div class="flex flex-col ">
+      <label for="name" class="font-bold">
+        name:
       </label>
-      <input type="text" name="name" id="name"  class="border-2"">
+      <input type="text" name="name" id="name"  class="border-2 rounded-md p-1 pl-2">
     </div>
 
-    <div class="flex items-center gap-2">
-      <label for="username">
-        username
+    <div class="flex flex-col">
+      <label for="username" class="font-bold">
+        username :
       </label>
-      <input type="text" name="username" id="username"  class="border-2">
+      <input type="text" name="username" id="username"   class="border-2 rounded-md p-1 pl-2">
     </div>
 
-    <div class="flex items-center gap-2">
-      <label for="password">
-        passsword
+    <div class="flex flex-col ">
+      <label for="password" class="font-bold">
+        passsword :
       </label>
-      <input type="password" name="password" id="password"  class="border-2">
+      <input type="password" name="password" id="password"   class="border-2 rounded-md p-1 pl-2">
     </div>
 
-    <div class="col-span-2 gap-2">
-      <label for="address">
-        address
+    <div class="flex flex-col "">
+      <label for="address" class="font-bold">
+        address :
       </label>
-      <textarea name="address" id="address" class="border-2 w-full">
+      <textarea name="address" id="address"  class="border-2 rounded-md p-1 pl-2">
       </textarea>
     </div>
 
-    <div class="flex items-center gap-2">
-      <label for="mobile">
-        mobile
+    <div class="flex flex-col ">
+      <label for="mobile" class="font-bold">
+        mobile :
       </label>
-      <input type="tel" name="mobile"  class="border-2">
+      <input type="tel" name="mobile"   class="border-2 rounded-md p-1 pl-2">
     </div>
 
 
-    <div class="flex items-center gap-2">
-      <label for="email
-      ">email</label>
-      <input type="email" name="email"  class="border-2">
+    <div class="flex flex-col">
+      <label for="email" class="font-bold">email</label>
+      <input type="email" name="email"   class="border-2 rounded-md p-1 pl-2">
     </div>
 
     
     <div class="cols-span-2">
       <button type="submit"
-              class="bg-red-200 p-2 rounded-md hover:bg-red-300 h-fit"> 
+              class="bg-red-200 p-2 rounded-md hover:bg-red-300 h-fit
+                     font-bold"> 
         + Add user
       </button>
     </div>
